f916ca93e8da1d13890727a791b7edacea5a67e0
stefmarien95 <56428152+stefmarien95@users.noreply.github.com>
2019-12-20 15:24:04 +0100

    SplashScreen+ logo

--- app/src/main/java/be/thomasmore/concertmanager/MainActivity.java
+++ app/src/main/java/be/thomasmore/concertmanager/MainActivity.java
@@ -2,6 +2,7 @@ package be.thomasmore.concertmanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -15,12 +16,16 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
     }
 
     @Override
